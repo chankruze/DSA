4266917798142c3e4e4a3321fe0c1575095fa1b9
chankruze <chankruze@gmail.com>
2021-01-05 19:46:11 +0530

    Update linear array
    Signed-off-by: chankruze <chankruze@gmail.com>

--- array/array_linear.c
+++ array/array_linear.c
@@ -6,28 +6,19 @@ Copyright (c) Geekofia 2020 and beyond
 */
 #include <stdio.h>
 
-#define clear() printf("\e[1;1H\e[2J")
-#define array_size(x) (sizeof(x) / sizeof((x)[0]))
-#define print_arr(arr, len)            \
-    printf("[");                       \
-    for (size_t i = 0; i < len; ++i) { \
-        if (len - 1 == i) {            \
-            printf("%d", arr[i]);      \
-        } else {                       \
-            printf("%d, ", arr[i]);    \
-        }                              \
-    }                                  \
-    printf("]\n")
-
+#include "../libs/chankruze.h"
 
 // main
 int main(int argc, char const *argv[]) {
     // clear screen
     clear();
-    // int array of 10 elements
-    int arr[5] = {};
-    // length of an int array
-    int len = array_size(arr);
+    int len = 5;
+    printf("[INPUT] Enter the length for the array: ");
+    scanf("%d", &len);
+    // int array of n elements
+    int arr[len];
+    // int arr[len] = {}; // will raise `variable-sized object may not be
+    // initialized` error length of an int array int len = array_size(arr);
     // print
     printf("[I] Length of Array = %d\n", len);
 
@@ -47,7 +38,7 @@ int main(int argc, char const *argv[]) {
 
     for (size_t i = 0; i < len; ++i) {
         int old = arr[i];
-        printf("Set new value for index %d: ", i);
+        printf("[INPUT] Set new value for index %ld: ", i);
         scanf("%d", &arr[i]);
         printf("index: %ld\told_value: %d\tnew_value: %d\n\n", i, old, arr[i]);
     }
@@ -57,11 +48,12 @@ int main(int argc, char const *argv[]) {
     /*
     => Inserting element in linear array
     */
+    // insert at index (index = position - 1)
     printf("\n[*] Inserting an element in linear array\n\n");
-    printf("Enter value to insert: ");
+    printf("[INPUT] Enter a value to insert: ");
     int new;
     scanf("%d", &new);
-    printf("Enter position to insert: ");
+    printf("[INPUT] Enter a position to insert: ");
     int position;
     scanf("%d", &position);
 
@@ -76,8 +68,9 @@ int main(int argc, char const *argv[]) {
     /*
     => Deleting element in linear array
     */
-    // delete by index
-    printf("Enter the postion of the element: ");
+    // delete by index (index = position - 1)
+    printf("\n[*] Deleting an element in linear array\n\n");
+    printf("[INPUT] Enter the postion of the element to delete: ");
     int delPos;
     scanf("%d", &delPos);
 
