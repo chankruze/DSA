3c08a98059ca24abb19c0af018bc1d7d3bb746bc
chankruze <chankruze@gmail.com>
2021-01-14 01:33:00 +0530

    [Linked List] Code Refactoring - Remove Unnecessary Variables
    Signed-off-by: chankruze <chankruze@gmail.com>

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -13,10 +13,6 @@ struct Node {
     struct Node* link;
 };
 
-// void printNode(struct Node* nodePtr) {
-//     printf("%d", nodePtr->data);
-// }
-
 void printNodeWithIndex(struct Node* nodePtr, size_t index) {
     printf("\n");
     printf("Position: %ld\n", index);
@@ -25,27 +21,25 @@ void printNodeWithIndex(struct Node* nodePtr, size_t index) {
 }
 
 void printLinkedList(struct Node* headerNode) {
-    struct Node* temp = headerNode;
     size_t index = 0;
 
-    while (temp->link != NULL) {
+    while (headerNode->link != NULL) {
         // header node have no data to print
-        temp = temp->link;
+        headerNode = headerNode->link;
         // print node data
-        printf("%d ", temp->data);
+        printf("%d ", headerNode->data);
     }
     printf("\n");
 }
 
 void printLinkedListDetailed(struct Node* headerNode) {
-    struct Node* temp = headerNode;
     size_t index = 0;
 
-    while (temp->link != NULL) {
+    while (headerNode->link != NULL) {
         // header node have no data to print
-        temp = temp->link;
+        headerNode = headerNode->link;
         // print node data
-        printNodeWithIndex(temp, ++index);
+        printNodeWithIndex(headerNode, ++index);
     }
 }
 
@@ -67,39 +61,33 @@ void addNodeAtTheEnd(struct Node* headerNode, int data) {
     newNode->link = NULL;  // end node link
 
     // 2. traverse entire list to find the last node
-    //  hold node temporarily to traverse the list
-    struct Node* lastNode = headerNode;
     // traverse for the list end
-    while (lastNode->link != NULL) {
+    while (headerNode->link != NULL) {
         // header node have no data to print
-        lastNode = lastNode->link;
+        headerNode = headerNode->link;
     }
 
     // 3. update last node's link to point to new node
-    lastNode->link = newNode;
+    headerNode->link = newNode;
 }
 
 void addNodeAtTheBeginning(struct Node* headerNode, int data) {
     /**
      * Logic
      *
-     * 1. store header node's link (1st node) to a temp variable
-     * 2. create a new node with asked data, link = temp var
-     * 3. fix header node to point to new node's address
+     * 1. create a new node with asked data, link = temp var
+     * 2. fix header node to point to new node's address
      *
      */
 
-    // 1. store header node's link (1st node) to a temp variable
-    struct Node* temp = headerNode;
-
-    // 2. create a new node with asked data, link = temp var
+    // 1. create a new node with asked data, link = temp var
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     // update new node's data
     newNode->data = data;
     // fix new nodes link
     newNode->link = headerNode->link;  // end node link
 
-    // 3. fix header node to point to new node's address
+    // 2. fix header node to point to new node's address
     headerNode->link = newNode;
 }
 
@@ -121,15 +109,13 @@ void addNodeAtNthPosition(struct Node* headerNode, size_t n, int data) {
 
     // 2. traverse the list to (n-1)th node, update new node's link to (n-1)th
     // node's link
-    // hold node temporarily to traverse the list upto (n-1)th node
-    struct Node* nMinus1thNode = headerNode;
     // traverse for the (n-1)th node
     for (size_t i = 0; i < n - 1; ++i) {
-        nMinus1thNode = nMinus1thNode->link;
+        headerNode = headerNode->link;
     }
     // update new node's link to (n-1)th node's link (nth node's address)
-    newNode->link = nMinus1thNode->link;
+    newNode->link = headerNode->link;
 
     // 3. fix nth node's link to point to new node's address
-    nMinus1thNode->link = newNode;
+    headerNode->link = newNode;
 }
